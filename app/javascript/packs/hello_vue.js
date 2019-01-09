24b0bc59dd2d5e562d19b3d53c8fe68191eb5b43
taiga39 <ishikawa.tigers@gmail.com>
2019-01-09 16:23:45 +0900

    Bmi 計算のやつ完成

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -32,6 +32,12 @@ new Vue({
   }
 })
 
+
+
+
+
+
+
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
 // need to always use single file components.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get "day1" => "language#index"
+  get "day2" => "home#bmi"
 end
