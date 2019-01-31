3e26e87d2f6807c36ef037f153efdc2f2c62cdbf
taiga39 <ishikawa.tigers@gmail.com>
2019-02-01 00:55:19 +0900

    データ表示

--- app/javascript/packs/useapi.js
+++ app/javascript/packs/useapi.js
@@ -9,31 +9,31 @@ var apiget2 = ",jp&units=metric&appid=e509537c974824c3ca874ebac7efe7b4"
 var app = new Vue({
   el: '#content15',
   data: {
+    message: "Osaka",
     city: null,
     temp: null,
-    message: "Osaka",
     condition: {
       main: null
     }
   },
-  // mounted: function(){
-  //   axios.get(apiget + this.message + apiget2)
-  //   .then(function(response){
-  //     this.city = response.data.name
-  //     this.temp = response.data.main.temp
-  //     this.condition = response.data.weather[0]
-  //   }.bind(this))
-  //   .catch(function(error){
-  //     console.log(error)
-  //   })
-  // },
+  mounted: function(){
+    axios.get(apiget + this.message + apiget2)
+    .then(function(response){
+      this.city = response.data.name
+      this.temp = response.data.main.temp
+      this.condition = response.data.weather[0]
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+    })
+  },
   filters: {
     roundUp(value){
       return Math.ceil(value)
     }
   },
   methods: {
-    greet: function (event) {
+    weather: function (event) {
       axios.get(apiget + this.message + apiget2)
       .then(function(response){
         this.city = response.data.name
@@ -43,7 +43,6 @@ var app = new Vue({
       .catch(function(error){
         console.log(error)
       })
-      alert(apiget + this.message + apiget2)
     }
   }
 })
