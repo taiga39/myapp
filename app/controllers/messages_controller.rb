a53f9cf8ddd42725895844064c28388c8da7b7d7
taiga39 <ishikawa.tigers@gmail.com>
2019-02-07 13:08:03 +0900

    aaa

--- app/controllers/messages_controller.rb
+++
@@ -1,6 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-    @messages = Message.all
-    @message = Message.new(username: 'taro')
-  end
-end
--- db/seeds.rb
+++
@@ -1,4 +0,0 @@
-Message.create(username: 'taro', body: 'こんにちは！太郎です、よろしくね。')
-Message.create(username: 'hanako', body: 'はじめまして、私は花子と申します。太郎さんは彼女とかいるんですか？')
-Message.create(username: 'taro', body: 'そうですね、特定の人はいませんが、花子が本命ですよ')
-Message.create(username: 'hanako', body: '面白いやつだな、気に入った。殺すのは最後にしてやる')
